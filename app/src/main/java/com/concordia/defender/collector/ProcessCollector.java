3b75a296452b4cf068b12e49569a7b6911ed4d7c
RogelioPumajulca <alropusa2@gmail.com>
2023-11-11 23:38:59 -0500

    1st

---
+++ app/src/main/java/com/concordia/defender/collector/ProcessCollector.java
@@ -0,0 +1,40 @@
+package com.concordia.defender.collector;
+
+
+
+import java.util.List;
+
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningAppProcessInfo;
+import android.content.Context;
+
+import com.concordia.defender.defender.DefenderDBHelper;
+import com.concordia.defender.defender.DefenderTask;
+import com.concordia.defender.model.Process;
+
+public class ProcessCollector extends DefenderTask {
+
+    public ProcessCollector(){
+        this.RunEvery = 5;
+    }
+
+    public void doWork(Context context) {
+        ActivityManager actManager = (ActivityManager) context
+                .getSystemService(Context.ACTIVITY_SERVICE);
+        List<RunningAppProcessInfo> runningProcessList = actManager
+                .getRunningAppProcesses();
+        DefenderDBHelper defDBHelper = DefenderDBHelper.getInstance(context);
+
+        // get running processes
+        for (RunningAppProcessInfo pInfo : runningProcessList) {
+            Process p = new Process();
+
+            p.TimeStamp = System.currentTimeMillis();
+            p.Uid = String.valueOf(pInfo.uid);
+            p.Pid = String.valueOf(pInfo.pid);
+            p.Name = pInfo.processName;
+
+            defDBHelper.insertProcess(p);
+        }
+    }
+}
